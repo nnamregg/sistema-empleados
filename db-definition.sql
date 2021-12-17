8136327dcb54064327d3e508ffe1f8b0d6c8fff3
nnamregg <nnamreg.t@gmail.com>
2021-12-17 12:00:38 -0300

    Agrego definición de DB

---
+++ db-definition.sql
@@ -0,0 +1,11 @@
+create database if not exists sistema;
+use sistema;
+drop table if exists empleados;
+
+create table if not exists empleados (
+	id int not null auto_increment,
+    nombre varchar(255),
+    correo varchar(255),
+    foto varchar(5000),
+    primary key(id)
+);
