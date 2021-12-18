6c9a006fe73dca05b5dcaaf34af55ff8adedacaa
nnamregg <nnamreg.t@gmail.com>
2021-12-18 11:20:51 -0300

    Configuro clases dinámicas del navbar-link

--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,13 @@
+// Clases dinámicas para links de navbar
+const navLinks = document.querySelectorAll(".nav-link")
+for (let link of navLinks) {
+    if (link.href == window.location.href) {
+       link.classList.add('active')
+       link.setAttribute('aria-current','page')
+    }
+}
+
+// Prompt de confirmación delete()
 const btnsConfirm = document.querySelectorAll("#btnBorrar")
 
 if (btnsConfirm.length) {
