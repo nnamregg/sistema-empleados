8b45a08c6d0aad053978bead215f5648bf3a7161
nnamregg <nnamreg.t@gmail.com>
2021-12-18 17:52:40 -0300

    Quito prompt de confirmacion delete

--- src/static/index.js
+++ src/static/index.js
@@ -1,20 +1,9 @@
 // Clases dinámicas para links de navbar
 const navLinks = document.querySelectorAll(".nav-link")
+
 for (let link of navLinks) {
     if (link.href == window.location.href) {
        link.classList.add('active')
        link.setAttribute('aria-current','page')
-    }
-}
-
-// Prompt de confirmación delete()
-const btnsConfirm = document.querySelectorAll("#btnBorrar")
-
-if (btnsConfirm.length) {
-    for (const btn of btnsConfirm) {
-        btn.addEventListener("click", event => {
-            const resp = confirm("Esta opción no tiene marcha atrás")
-            if (!resp) event.preventDefault()
-        })
     }
 }
